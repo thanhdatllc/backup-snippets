67cce8b37171dceb545597c758ce23803b443b80
Thanh Dat LLC <thanhdatllc@outlook.com>
2016-04-10 09:39:03 +0700

    Backup Snippets v1

---
+++ BackupSnippets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackupSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                         ################################################################");
+            Console.WriteLine("                         #                    BACKUP DATA SNIPPETS v1.0                 #");
+            Console.WriteLine("                         #                    Author: Thanh Dat                         #");
+            Console.WriteLine("                         #                    Company: Thanh Dat LLC                    #");
+            Console.WriteLine("                         ################################################################");
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine();
+            var month = DateTime.Today.Month.ToString();
+            var year = DateTime.Today.Year.ToString();
+            var day = DateTime.Today.Day.ToString();
+            var time = DateTime.Now.ToLongTimeString();
+            time = time.Replace(":", "-");
+            time = time.Replace(" ", "-");
+            var dirStatup = AppDomain.CurrentDomain.BaseDirectory;
+            var dirBackup = string.Format("{0}Snippets\\{1}-{2}", dirStatup, year, month);
+            var db = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Snippets\\snippets.sqlite");
+            var dbDest = string.Format("{0}\\snippets.sqlite-{1}-{2}-{3}-{4}", dirBackup, year, month, day, time);
+            if (File.Exists(db))
+            {
+                if (!Directory.Exists(dirBackup)) Directory.CreateDirectory(dirBackup);
+                File.Copy(db, dbDest);
+            }
+            Console.WriteLine("                         # Running backup data..........................................");
+            Thread.Sleep(3000);
+            Console.WriteLine("                         # Backup data completed! ......................................");
+            Thread.Sleep(1000);
+            Console.WriteLine("                         # Program will exit after 10s..................................");
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ BackupSnippets/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Backup Data Snippets")]
+[assembly: AssemblyDescription("Backup Data Snippets Console - Snippets.me")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thanh Dat LLC")]
+[assembly: AssemblyProduct("BackupSnippets")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Thanh Dat LLC")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11b9a3c4-e10a-48b9-9a89-f0fa5ee1c892")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
